691f70f8de32e020fb85b51664d9907c46207c9b
ChrisBadetZaitane <christophe.badet13@gmail.com>
2016-05-27 15:24:03 +0200

    ajout champion's league

---
+++ ChampionsLeague.java
@@ -0,0 +1,21 @@
+package applisynthese;
+/**
+ *
+ * @author Antoine Daigremont
+ */
+public class ChampionsLeague extends EuropeanTournament{
+    /**
+     * @override
+     */
+    public void checkRefereeNationality(){
+    
+    }
+    
+     /**
+     * @override
+     */
+    public int groupRanking(){
+        
+        return 0;
+    }
+}
