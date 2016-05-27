67c354ac1cf8949b9c4e30bdca259e858fba3f5d
Mattowskii <mattowski@localhost.localdomain>
2016-05-27 11:27:56 +0200

    firstcommit

---
+++ competition.java
@@ -0,0 +1,42 @@
+
+package applisynthese;
+
+import java.lang.*;
+import java.util.Date;
+
+
+
+
+
+public class competition{
+    
+      private String competitionName;
+      private Date year;
+      private Team calendar;
+      private Team Winner;
+      
+      public int getTeamRank( Team teamname){
+
+          
+      }
+      
+      public int[][] getLeagueTable (String competionName){
+      
+      }
+
+      public Team getWinner ( String competionName){
+
+      }
+
+      public Team[] getAllWinners ( String competionName){
+
+      }
+
+      public void closeChampionship ( String competionName){
+
+      }
+
+      public void setMatch ( Team team1, Team team2, String competitionName, Date dateMatch){
+
+      }
+}
