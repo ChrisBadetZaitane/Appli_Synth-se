95b72d746717f593f5846c36428b075b646715e2
ChrisBadetZaitane <christophe.badet13@gmail.com>
2016-05-27 11:31:23 +0200

    suppression dossier

--- Applisynthese/src/applisynthese/Championship.java
+++
@@ -1,9 +0,0 @@
-package applisynthese;
-import java.lang.*;
-/**
- *
- * @author Antoine Daigremont
- */
-public abstract class Championship extends Competition{
-    
-}
--- Applisynthese/src/applisynthese/Div2.java
+++
@@ -1,9 +0,0 @@
-package applisynthese;
-import java.lang.*;
-/**
- *
- * @author Antoine Daigremont
- */
-public abstract class Div2 extends Championship{
-   
-}
