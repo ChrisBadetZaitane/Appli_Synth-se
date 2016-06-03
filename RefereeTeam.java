92d91eb3758420635b6d80cee597ff8a0f723e52
fanny.rottee@gmail.com <martin.bolot@etu.univ-lyon1.fr>
2016-06-03 09:16:54 +0200

    Mets à jour la classe RefereeTeam

--- RefereeTeam.java
+++ RefereeTeam.java
@@ -11,45 +11,25 @@ package applisynthese;
  */
 public class RefereeTeam {
 
-    public RefereeTeam(int referee, int assistant1, int assistant2, int assistant3, int assistant4) {
-        this.referee = referee;
-        this.assistant1 = assistant1;
-        this.assistant2 = assistant2;
-        this.assistant3 = assistant3;
-        this.assistant4 = assistant4;
+    public RefereeTeam(String teamName, int nationality) {
+        this.teamName = teamName;
+        this.nationality = nationality;
+    }
+      
+    public int getRefereeTeamId() {
+        return refereeTeamId;
     }
 
-    public RefereeTeam() {
-        this.referee = referee;
-        this.assistant1 = assistant1;
-        this.assistant2 = assistant2;
-        this.assistant3 = assistant3;
-        this.assistant4 = assistant4;
-    }
-    public int getReferee() {
-        return referee;
+    public String getTeamName() {
+        return teamName;
     }
 
-    public int getAssistant1() {
-        return assistant1;
-    }
-
-    public int getAssistant2() {
-        return assistant2;
-    }
-
-    public int getAssistant3() {
-        return assistant3;
-    }
-
-    public int getAssistant4() {
-        return assistant4;
+    public int getNationality() {
+        return nationality;
     }
     
-    private int referee;
-    private int assistant1;
-    private int assistant2;
-    private int assistant3;
-    private int assistant4;
+    private int refereeTeamId;
+    private String teamName;
+    private int nationality;
     
 }
